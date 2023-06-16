d5fdc6bbdcca44bc5edf4cd30af9c9b17fc95c8d
Hollis <blueartlover6@gmail.com>
2023-06-15 19:16:34 -0600

    Added components that build home page and added some images and basic styling

--- vidspace/src/Components/Home.js
+++ vidspace/src/Components/Home.js
@@ -1,4 +1,9 @@
 import React from "react";
+import MainContent from "./MainContent";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import logo from "../Images/VidSpace.png";
+import doggo from "../Images/doggo.jpg";
 
 const Home = () => {
   return (
@@ -6,43 +11,34 @@ const Home = () => {
       {/* Header */}
       <header className="flex items-center justify-between px-4 py-2 bg-white">
         <div className="flex items-center">
-          {/* YouTube logo */}
-          <img
-            src="/path/to/your-logo.png"
-            alt="YouTube Logo"
-            className="h-8 w-auto"
-          />
+          {/* VidSpace logo */}
+          <img src={logo} alt="VidSpace Logo" className="h-16 w-auto" />
           {/* Search bar */}
           <input
             type="text"
             placeholder="Search"
-            className="ml-4 px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+            className="ml-4 px-6 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
           />
         </div>
         {/* User avatar */}
         <div className="flex items-center">
           <img
-            src="/path/to/your-avatar.png"
+            src={doggo}
             alt="User Avatar"
-            className="h-8 w-8 rounded-full"
+            className="h-16 w-16 rounded-full"
           />
         </div>
       </header>
 
       {/* Main content */}
       <main className="flex flex-row">
-        {/* Sidebar */}
-        <aside className="w-1/4 p-4 bg-white">{/* Sidebar content */}</aside>
-
-        {/* Video feed */}
-        <div className="flex-grow p-4 bg-gray-200">
-          {/* Video feed content */}
-        </div>
+        <Sidebar />
+        <MainContent />
       </main>
 
       {/* Footer */}
       <footer className="flex items-center justify-center px-4 py-2 bg-white">
-        {/* Footer content */}
+        <Footer />
       </footer>
     </div>
   );
---
+++ vidspace/src/Components/MainContent.js
@@ -0,0 +1,35 @@
+import React from "react";
+import snek from "../Images/Snek.png";
+import thumbnail1 from "../Images/thumbnail1.jpg";
+
+const MainContent = () => {
+  return (
+    <div className="flex-grow p-4 bg-gray-200">
+      <div className="space-y-4">
+        <div className="flex items-center space-x-4">
+          <img src={snek} alt="Video Thumbnail" className="h-24 w-32" />
+          <div>
+            <h2 className="text-lg font-semibold text-gray-800">
+              There's a Snake in my Hotel
+            </h2>
+            <p className="text-sm text-gray-500">Blue Art Gaming</p>
+            {/* Add more video details */}
+          </div>
+        </div>
+        <div className="flex items-center space-x-4">
+          <img src={thumbnail1} alt="Video Thumbnail" className="h-24 w-32" />
+          <div>
+            <h2 className="text-lg font-semibold text-gray-800">
+              3 Scary Games #99
+            </h2>
+            <p className="text-sm text-gray-500">Markiplier</p>
+            {/* Add more video details */}
+          </div>
+        </div>
+        {/* Add more videos */}
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
