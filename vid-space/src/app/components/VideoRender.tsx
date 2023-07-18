55b757aad0658abd45102616c44d22eadb21dad0
Hollis <blueartlover6@gmail.com>
2023-07-17 20:40:39 -0600

    Set up Extremely Basic Front page

--- vid-space/src/app/components/VideoRender.tsx
+++ vid-space/src/app/components/VideoRender.tsx
@@ -1,123 +1,31 @@
-'use client'
-import { useState } from 'react';
+import React from 'react';
+import MainImg from "../Images/mcc.png"
+import Image from 'next/image';
 
-interface Video {
-  title: string;
-  image: string;
-    description: string;
-    link: string;
-    iframe: any;
-}
-
-//array for videos
-const allVideos: Video[] = [ 
-    {
-        title: '1',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    }, 
-    {
-        title: '2',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '3',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link:"https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '4',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '5',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '6',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '7',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '8',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link:  "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '9',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
-    {
-        title: '99',
-        image: "",
-        description: "In todays short video essay I discuss why the Minecraft civilization videos have become so popular.",
-        link: "https://www.youtube.com/watch?v=Dy1lN7DkSPw",
-        iframe: <iframe width="1903" height="760" src="https://www.youtube.com/embed/Dy1lN7DkSPw" title="Why Minecraft Civilization Videos are Captivating Audiences" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>,
-    },  
+// Dummy video data
+const videos = [
+  {
+    id: 1,
+    title: 'Cool Video 1',
+    thumbnailUrl: MainImg,
+  },
+  {
+    id: 2,
+    title: 'Awesome Video 2',
+    thumbnailUrl: MainImg,
+  },
 ];
 
-const getRandomVideos = (videos: Video[], count: number): Video[] => {
-  const shuffled = videos.sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, count);
-};
-
 const VideoRender: React.FC = () => {
-  const [videos, setVideos] = useState<Video[]>(getRandomVideos(allVideos, 10));
-
-  const handleRefresh = () => {
-    setVideos(getRandomVideos(allVideos, 10));
-  };
-
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {videos.map((video, index) => (
-          <a key={index} href={video.link} target="_blank" rel="noopener noreferrer">
-            <div
-              className="group rounded-lg border border-transparent p-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            >
-                    <h2 className="mb-2 text-xl font-semibold">{video.title}</h2>
-                    
-              <img src={video.image} alt={video.title} className="w-full h-36 object-cover rounded" />
-              <p className="mt-2 text-sm opacity-70">{video.description}</p>
-            </div>
-          </a>
-        ))}
-      </div>
-      <button
-        onClick={handleRefresh}
-        className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none"
-      >
-        Refresh
-      </button>
-    </main>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {videos.map((video) => (
+          <div key={video.id} className="bg-white p-4 rounded-lg shadow-md">
+              <Image src={video.thumbnailUrl} alt={video.title } width={600} height={600}/>
+          <h3 className="text-lg font-semibold">{video.title}</h3>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- vid-space/src/app/page.tsx
+++ vid-space/src/app/page.tsx
@@ -2,12 +2,28 @@ import VideoRender from "./components/VideoRender"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        
-          <VideoRender/>
+    <main className="flex min-h-screen flex-col">
+      {/* Search bar at the top */}
+      <div className="bg-gray-800 py-4 px-8 text-white flex justify-between items-center">
+        <input
+          type="text"
+          className="px-4 py-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+          placeholder="Search..."
+        />
+        {/* Add your search button or other search-related elements here */}
+      </div>
 
+      <div className="flex flex-grow">
+        {/* Collapsible bar to the left */}
+        <div className="bg-gray-200 p-4 w-1/6">
+          {/* Add your collapsible content here */}
+          {/* Example: <p>Collapsible Content</p> */}
+        </div>
 
+        {/* Centered VideoRender */}
+        <div className="flex flex-grow justify-center items-center">
+          <VideoRender />
+        </div>
       </div>
     </main>
   )
