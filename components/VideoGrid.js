4f3f323a4bb1a3320a11ad5b3ad9d05a50cfc626
Hollis <blueartlover6@gmail.com>
2023-09-18 11:24:57 -0600

    Finished and added articles page + content

--- components/VideoGrid.js
+++ components/VideoGrid.js
@@ -9,6 +9,7 @@ const galleryData = [
     likeCount: 10,
     viewCount: 1000,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
     title: "Why people love science fiction",
@@ -16,6 +17,7 @@ const galleryData = [
     likeCount: 15,
     viewCount: 1500,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
     title: "How to stay motivated",
@@ -23,6 +25,7 @@ const galleryData = [
     likeCount: 5,
     viewCount: 500,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
     title: "Why cats are cute",
@@ -30,6 +33,7 @@ const galleryData = [
     likeCount: 20,
     viewCount: 2000,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
     title: "Farming Pumpkins",
@@ -37,6 +41,7 @@ const galleryData = [
     likeCount: 8,
     viewCount: 800,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
     title: "Why dystopias captivate audiences ",
@@ -44,6 +49,7 @@ const galleryData = [
     likeCount: 12,
     viewCount: 1800,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
     title: "Medieval practices",
@@ -51,6 +57,7 @@ const galleryData = [
     likeCount: 8,
     viewCount: 800,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
     title: "Could power armor work?",
@@ -58,62 +65,31 @@ const galleryData = [
     likeCount: 12,
     viewCount: 1800,
     ContentType: "video",
+    link: "VideoPage",
   },
   {
-    title: "Exploring the topic of space earthquakes",
-    url: "/Images/thumbnail-5.png",
-    likeCount: 10,
-    viewCount: 1000,
+    title: "How Astronauts do space walks",
+    url: "/Images/thumbnail-9.png",
+    likeCount: 23,
+    viewCount: 456,
     ContentType: "article",
+    link: "ArticlePage",
   },
   {
-    title: "Why people love science fiction",
-    url: "/Images/thumbnail-1.png",
-    likeCount: 15,
-    viewCount: 1500,
+    title: "Why our galaxy is amazing",
+    url: "/Images/thumbnail-10.png",
+    likeCount: 65,
+    viewCount: 2345,
     ContentType: "article",
+    link: "ArticlePage",
   },
   {
-    title: "How to stay motivated",
-    url: "/Images/thumbnail-2.png",
-    likeCount: 5,
-    viewCount: 500,
+    title: "Being humbled by cosmic perspective",
+    url: "/Images/thumbnail-11.png",
+    likeCount: 234,
+    viewCount: 6544,
     ContentType: "article",
-  },
-  {
-    title: "Why cats are cute",
-    url: "/Images/thumbnail-3.png",
-    likeCount: 20,
-    viewCount: 2000,
-    ContentType: "article",
-  },
-  {
-    title: "Farming Pumpkins",
-    url: "/Images/thumbnail-4.png",
-    likeCount: 8,
-    viewCount: 800,
-    ContentType: "article",
-  },
-  {
-    title: "Why dystopias captivate audiences ",
-    url: "/Images/thumbnail-6.png",
-    likeCount: 12,
-    viewCount: 1800,
-    ContentType: "article",
-  },
-  {
-    title: "Medieval practices",
-    url: "/Images/thumbnail-7.png",
-    likeCount: 8,
-    viewCount: 800,
-    ContentType: "article",
-  },
-  {
-    title: "Could power armor work?",
-    url: "/Images/thumbnail-8.png",
-    likeCount: 12,
-    viewCount: 1800,
-    ContentType: "article",
+    link: "ArticlePage",
   },
 ];
 
@@ -154,7 +130,7 @@ const VideoGrid = () => {
             key={index}
             className="m-4 p-4 border border-orange-200 rounded-lg text-center md:w-80 transition-transform transform hover:scale-110 hover:shadow-lg"
           >
-            <Link href="VideoPage">
+            <Link href={item.link}>
               <Image src={item.url} width={800} height={800} alt={item.title} />
               <h2 className="mt-2 text-3xl text-orange-600">{item.title}</h2>
               <div className="flex justify-between text-orange-800 mt-2 text-xl">
--- pages/ArticlePage.js
+++ pages/ArticlePage.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
 import VideoGrid from "../components/VideoGrid";
+import Image from "next/image";
 
 export default function ArticlePage() {
   return (
@@ -16,17 +17,93 @@ export default function ArticlePage() {
         <Navbar />
         <div className="bg-black pb-1">
           <div className="bg-gray-800 rounded-xl pt-10 m-12 pb-10 border-2 border-red-600">
-            <h2 className="mt-2 pb-4 text-4xl text-center text-orange-600">
-              Doggo
+            <h2 className="mt-2 pb-4 text-6xl text-center text-orange-600">
+              The Topic of Space Stuff
             </h2>
-            <p>
-              This is eventually where a entire aticle will be it will be
-              formated and look good too!
+            <h1 className="text-4xl text-center text-orange-600 my-8">
+              Exploring the Vast Universe
+            </h1>
+            <p className="text-center text-orange-600 text-xl">
+              The universe, with its boundless expanse, has always fascinated
+              humanity. From the twinkling stars in the night sky to the
+              enigmatic black holes that lurk in the cosmic depths, space is a
+              topic that never ceases to amaze us.
             </p>
+            <Image
+              src="/Images/thumbnail-11.png"
+              width={800}
+              height={800}
+              className="md:w-1/4 p-4 m-auto "
+            />
+
+            <h1 className="text-4xl text-center text-orange-600 my-8">
+              The Wonders of Our Solar System
+            </h1>
+            <p className="text-center text-orange-600 text-xl">
+              Within our own cosmic neighborhood lies the mesmerizing solar
+              system. From the scorching surface of Mercury to the icy plains of
+              Pluto, each celestial body has its own unique characteristics and
+              mysteries waiting to be unraveled.
+            </p>
+            <Image
+              src="/Images/thumbnail-10.png"
+              width={800}
+              height={800}
+              className="md:w-1/4 p-4 m-auto"
+            />
+
+            <h1 className="text-4xl text-center text-orange-600 my-8">
+              The Quest for Extraterrestrial Life
+            </h1>
+            <p className="text-center text-orange-600 text-xl">
+              One of the most intriguing questions in the realm of space
+              exploration is the search for extraterrestrial life. Scientists
+              are constantly scanning the cosmos for signs of life beyond Earth,
+              with missions to Mars and distant exoplanets offering tantalizing
+              clues.
+            </p>
+            <Image
+              src="/Images/thumbnail-9.png"
+              width={800}
+              height={800}
+              className="md:w-1/4 p-4 m-auto"
+            />
+
+            <h1 className="text-4xl text-center text-orange-600 my-8">
+              Journeying to the Stars
+            </h1>
+            <p className="text-center text-orange-600 text-xl">
+              Humanity's dreams of exploring the stars are becoming a reality.
+              Space agencies and private companies are planning missions to
+              distant stars and exoplanets, fueled by breakthroughs in
+              propulsion technology and interstellar travel concepts.
+            </p>
+            <Image
+              src="/Images/thumbnail-2.png"
+              width={800}
+              height={800}
+              className="md:w-1/4 p-4 m-auto"
+            />
+
+            <h1 className="text-4xl text-center text-orange-600 my-8">
+              The Future of Space Exploration
+            </h1>
+            <p className="text-center text-orange-600 text-xl">
+              As we venture further into the cosmos, the possibilities seem
+              endless. From colonizing other planets to harnessing the resources
+              of asteroids, the future of space exploration promises to be an
+              exciting journey of discovery and innovation.
+            </p>
+            <Image
+              src="/Images/thumbnail-5.png"
+              width={800}
+              height={800}
+              className="md:w-1/4 p-4 m-auto"
+            />
           </div>
           <div className="bg-gray-800 rounded-xl pt-12 m-12 border-2 border-orange-600">
             <h1 className="mt-2 pb-4 text-5xl text-center text-orange-600">
-              More Videos
+              More Content
             </h1>
             <VideoGrid />
           </div>
